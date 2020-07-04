c815e75f378283400ffe4f403f518017e4ae2db6
Your Name <hoangpn314@gmail.com>
2020-07-04 12:28:49 +0700

    abc

--- src/Rabin_Karp/Main.java
+++ src/Rabin_Karp/Main.java
@@ -2,10 +2,11 @@ package Rabin_Karp;
 
 //https://nhannguyen95.github.io/rabin-karp-algorithm/
 public class Main {
-    static final int BASE = 2;
-    static final int PRIME = 101;
-    static int valueCurrent;
+    static final int BASE = 3;
+    static final int PRIME = 517;
+    static long valueCurrent;
     static int hashCurrent;
+    static long valueOrigin;
     static int hashOrigin;
     static String pattern = "ABABCABAB";
     static String text = "ABABDABACDABABCABAB";
@@ -15,12 +16,12 @@ public class Main {
         hashOrigin = initHashP(pattern);
         hashCurrent = initHashT(text.substring(0, pattern.length()));
 
-        System.out.println(pattern + " : " + hashOrigin);
-        System.out.println(text.substring(0, pattern.length()) + " : " + hashCurrent);
+        System.out.println(pattern + " : " + hashOrigin+":"+valueOrigin);
+        System.out.println(text.substring(0, pattern.length()) + " : " + hashCurrent+":"+valueCurrent);
 
 
-        for (int i = 1; i < text.length(); i++) {
-            hash(text.charAt(i), text.charAt(i + pattern.length()-1));
+        for (int i = 1; i < text.length() - pattern.length() + 1; i++) {
+            hash(text.charAt(i - 1), text.charAt(i + pattern.length() - 1));
             System.out.println(text.substring(i, i + pattern.length()) + " : " + hashCurrent);
         }
     }
@@ -29,11 +30,11 @@ public class Main {
         char[] temp = init.toCharArray();
         int mul = 1;
         for (int i = temp.length - 1; i >= 0; i--) {
-            valueCurrent += temp[i] * mul;
+            valueOrigin += temp[i] * mul;
             mul *= BASE;
         }
-        hashCurrent = valueCurrent % PRIME;
-        return hashCurrent;
+        hashOrigin = (int) (valueOrigin % PRIME);
+        return hashOrigin;
     }
 
     private static int initHashT(String init) {
@@ -43,13 +44,12 @@ public class Main {
             valueCurrent += temp[i] * mul;
             mul *= BASE;
         }
-        hashCurrent = valueCurrent % PRIME;
+        hashCurrent = (int) (valueCurrent % PRIME);
         return hashCurrent;
     }
 
     private static void hash(char old, char next) {
-        valueCurrent = (int) ((valueCurrent - old * Math.pow(BASE, pattern.length())) * BASE + next);
-
-        hashCurrent = valueCurrent % PRIME;
+        valueCurrent = (int) ((valueCurrent - old * Math.pow(BASE, pattern.length()-1)) * BASE + next);
+        hashCurrent = (int) (valueCurrent % PRIME);
     }
 }
